ac0c5bc82d4229a77278fe1fa4e963649b858dca
jesus-reyes2008 <dr2024114@inti.edu.sv>
2024-08-19 14:19:36 -0600

    inicio gifhub
    hola

---
+++ 0.3-comparación.py
@@ -0,0 +1,11 @@
+#operadores de comparación 
+
+num1 = 10;
+num2 = 2.5;
+texto = "hola";
+
+#mayor que 
+print(num1 > num2);
+
+#menor que
+print(num1 < num2);
---
+++ 0.5-input.py
@@ -0,0 +1,10 @@
+#input = nombre y mayor de edad 
+
+nombre = input("ingrese su nombre");
+
+edad = input("ingrese su edad");
+
+if edad <= 18;
+    print("eres mayor de edad");
+else:
+    print ("eres menor de edad");
