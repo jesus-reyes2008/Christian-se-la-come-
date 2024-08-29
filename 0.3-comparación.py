86c85ce93cb5b1d76811f5bcc535a4ad05aedf54
jesus-reyes2008 <dr2024114@inti.edu.sv>
2024-08-29 08:01:54 -0600

    arregle el codigo
    A

--- 0.3-comparación.py
+++ 0.3-comparación.py
@@ -1,11 +1,15 @@
 #operadores de comparación 
 
-num1 = 10;
-num2 = 2.5;
-texto = "hola";
+num1 = 10
+num2 = 2.5
+texto = "hola"
 
 #mayor que 
-print(num1 > num2);
+print(num1 > num2)
 
 #menor que
-print(num1 < num2);
+print(num1 < num2)
+
+print(texto)
+
+#para que ingresas una variable sin usarla, todo macaco y bot
--- 0.5-input.py
+++ 0.5-input.py
@@ -1,10 +1,13 @@
 #input = nombre y mayor de edad 
 
-nombre = input("ingrese su nombre");
+nombre = input("ingrese su nombre")
 
-edad = input("ingrese su edad");
+edad = int(input("ingrese su edad"))
 
-if edad <= 18;
-    print("eres mayor de edad");
+if edad > 18:
+    print("eres mayor de edad")
 else:
-    print ("eres menor de edad");
+    print ("eres menor de edad")
+
+#no saben programar o que? Pasate a gastronomia
+#ya te lo arregle macaco
